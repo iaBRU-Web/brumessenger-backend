98407fc6be5f6096a8ce1222027fe97775d4ffeb
iaBRU-Web <inezaimebrunos3a@gmail.com>
2026-02-15 18:02:17 +0200

    Update get-users.js

--- api/get-users.js
+++ api/get-users.js
@@ -2,10 +2,10 @@
 // Get all users
 // Created by: Ineza Aime Bruno
 
-// Simple in-memory storage (same as auth.js)
-let users = {};
+const users = {};
 
 export default async function handler(req, res) {
+  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -15,10 +15,14 @@ export default async function handler(req, res) {
   }
   
   if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ 
+      success: false,
+      error: 'Method not allowed' 
+    });
   }
   
   try {
+    // Convert users object to array
     const userList = Object.values(users);
     
     return res.status(200).json({ 
@@ -28,6 +32,7 @@ export default async function handler(req, res) {
   } catch (error) {
     console.error('Get users error:', error);
     return res.status(500).json({ 
+      success: false,
       error: 'Server error',
       details: error.message 
     });
